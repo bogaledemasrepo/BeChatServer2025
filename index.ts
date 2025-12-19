c8159dfe0e34b8e613c95ebe6e73f1807cf3851f
bogidemasrepo <bogidemas@gmail.com>
2025-12-19 11:43:34 +0300

    cron added for automatic request to server inorder to block render down time

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const SERVER_API_URL = process.env.API_URL || "http://localhost:8000/api";
+const cronJob = require('./lib/cron').default;
 
 app.use(express.static('public'));
 
@@ -11,8 +13,8 @@ const handleHealth = (req: Request, res: Response) => {
   res.status(200).json({success:true})
 }
 
-app.get("/health",handleHealth)
-
-app.listen(port,() => {
+app.get(SERVER_API_URL + "/health", handleHealth);
+cronJob.start();
+app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
