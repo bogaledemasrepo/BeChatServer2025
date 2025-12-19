a4d4f979ad43f6f84928019f48e0b2c82f385f2d
bogidemasrepo <bogidemas@gmail.com>
2025-12-19 11:51:58 +0300

    url issue fixed

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-const SERVER_API_URL = process.env.API_URL || "http://localhost:8000/api";
 const cronJob = require('./lib/cron').default;
 
 app.use(express.static('public'));
@@ -13,8 +12,8 @@ const handleHealth = (req: Request, res: Response) => {
   res.status(200).json({success:true})
 }
 
-app.get(SERVER_API_URL + "/health", handleHealth);
+app.get(`/api/health`, handleHealth);
 cronJob.start();
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}/api/health`);
 });
