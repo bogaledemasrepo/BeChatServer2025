6ee64c5e91ab89b9ebddd700f20797c7e6b3409f
bogidemasrepo <bogidemas@gmail.com>
2025-12-19 11:36:33 +0300

    Envirment variable added and simple server setup with API health check implemented

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!" ,"How are you doing today?");
+import express, { type Request, type Response } from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+const handleHealth = (req: Request, res: Response) => {
+  res.status(200).json({success:true})
+}
+
+app.get("/health",handleHealth)
+
+app.listen(port,() => {
+  console.log(`Server running on http://localhost:${port}`);
+});
