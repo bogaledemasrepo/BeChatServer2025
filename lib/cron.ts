b99c38c5d3c5d88dbcf2b1eaf3bbf3b79e117c57
bogidemasrepo <bogidemas@gmail.com>
2025-12-20 23:08:17 +0300

    update crone jop url for dev and prod

--- lib/cron.ts
+++ lib/cron.ts
@@ -7,14 +7,14 @@ const runtimeEnv = process.env.RUNTIME_ENV || "production";
 
 const job = runtimeEnv === "development" ? new cron.CronJob("*/14 * * * *", function () {
   http
-    .get(process.env.API_URL!, (res) => {
+    .get(process.env.DEV_API_URL!, (res) => {
       if (res.statusCode === 200) console.log("Development ","GET request sent successfully");
       else console.log("GET request failed", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sending request", e));
 }) : new cron.CronJob("*/14 * * * *", function () {
   https
-    .get(process.env.API_URL!, (res) => {
+    .get(process.env.PROD_API_URL!, (res) => {
       if (res.statusCode === 200) console.log("Production ","GET request sent successfully");
       else console.log("GET request failed", res.statusCode);
     })
