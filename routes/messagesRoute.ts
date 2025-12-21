f7475c8d8abe6c38f8ee62a1b33bb2b74ee60f50
bogidemasrepo <bogidemas@gmail.com>
2025-12-21 06:02:31 +0300

    fix route param issue

--- routes/messagesRoute.ts
+++ routes/messagesRoute.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/authMiddleware';
-import { deleteMessage, markMessageAsRead, sendMessage, updateMessage } from '../controllers/messageService';
+import { deleteMessage, getMessages, markMessageAsRead, sendMessage, updateMessage } from '../controllers/messageService';
 
 const messagesRouter = express.Router();
 
-messagesRouter.post('/',authMiddleware,sendMessage)
+messagesRouter.post('/',authMiddleware,express.json(),sendMessage)
+.get("/:friendId",authMiddleware,express.json(),getMessages)
 .get('/markRead',authMiddleware,markMessageAsRead)
-.put('/:id',authMiddleware,updateMessage)
-.delete('/:id',authMiddleware,deleteMessage);
+.put('/:messageId',authMiddleware,express.json(),updateMessage)
+.delete('/:messageId',authMiddleware,deleteMessage);
 
 export default messagesRouter;
